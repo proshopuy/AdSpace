d381b325bbb957455b470db89ed7802a16aab60a
proshopuy <proshopuy@gmail.com>
2026-04-22 14:31:16 -0300

    fix Stripe type error

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -16,7 +16,7 @@ export async function POST(request: Request) {
   }
 
   if (event.type === "checkout.session.completed") {
-    const session = event.data.object as Stripe.CheckoutSession;
+    const session = event.data.object as Stripe.Checkout.Session;
     const spaceId = session.metadata?.space_id;
     const userId = session.metadata?.user_id;
 
